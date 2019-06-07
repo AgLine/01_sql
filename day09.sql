5d9eb12c23f8eb0f6e942074a4ef106a0daa0389
AgLine <rladmstjs120@gmail.com>
2019-06-07 17:20:30 +0900

    실습예제

--- day09.sql
+++ day09.sql
@@ -116,3 +116,147 @@ HAVING AVG(e.sal) >= 2000
 5. SELECT   절에 명시된 컬럼, 식만 출력
 7. ORDER BY 가 있다면 정렬 조건에 맞추어 최종 정령하여 결과 출력
 */
+
+-- 수업중 실습
+
+-- 1. 매니저별, 부하직원의 수를 구하고, 많은 순으로 정렬
+--   : mgr 컬럼이 그룹화 기준 컬럼
+SELECT e.mgr
+      ,COUNT(*) "부하직원 수"
+  FROM emp e 
+ GROUP BY e.mgr
+ ORDER BY "부하직원 수" DESC
+;
+/*
+MGR, 부하직원 수
+7698	5
+(null)  3
+7839	3
+7566	1
+7782	1
+7902	1
+*/
+-- 2.1 부서별 인원을 구하고, 인원수 많은 순으로 정렬
+--    : deptno 컬럼이 그룹화 기준 컬럼
+SELECT e.deptno 부서번호
+      ,COUNT(e.empno) 인원
+  FROM emp e
+ GROUP BY e.deptno
+ ORDER BY 인원 DESC
+;
+/*
+부서번호, 인원
+30	    6
+20	    3
+10	    3
+        2
+*/
+-- 2.2 부서 배치 미배정 인원 처리
+SELECT NVL(TO_CHAR(e.deptno),'직무 미배정') 부서배치
+  FROM emp e
+ GROUP BY e.deptno
+;
+/*
+부서배치
+30
+직무 미배정
+10
+20
+*/
+
+-- 3.1 직무별 급여 평균 구하고, 급여평균 높은 순으로 정렬
+--   : job 이 그룹화 기준 컬럼
+SELECT TO_CHAR(AVG(e.sal),'$9,999.99') 평균
+  FROM emp e
+ GROUP BY e.job
+ ORDER BY  평균 DESC
+;
+/*
+평균
+
+ $5,000.00
+ $3,000.00
+ $2,758.33
+ $1,400.00
+ $1,016.67
+*/
+-- 3.2 job 이 null 인 데이터 처리
+SELECT NVL(e.job,'직무 미배정') 직무
+  FROM emp e
+ GROUP BY e.job
+;
+/*
+직무
+CLERK
+SALESMAN
+ANALYST
+MANAGER
+직무 미배정
+PRESIDENT
+*/
+
+-- 4. 직무별 급여 총합 구하고, 총합 높은 순으로 정렬
+--   : job 이 그룹화 기준 컬럼
+SELECT SUM(e.sal) 총합
+  FROM emp e
+ GROUP BY e.job
+ ORDER BY 총합 DESC
+;
+/*
+총합
+
+8275
+5600
+5000
+3050
+3000
+*/
+-- 5. 급여의 앞단위가 1000미만, 1000, 2000, 3000, 5000 별로 인원수를 구하시오
+--    급여 단위 오름차순으로 정렬
+SELECT COUNT(CASE WHEN e.sal >= 5000 THEN 1 END) as "5" 
+      ,COUNT(CASE WHEN e.sal >= 3000 AND e.sal < 5000 THEN 1 END)  as "3"
+      ,COUNT(CASE WHEN e.sal >= 2000 AND e.sal < 3000 THEN 1 END)   as "2"
+      ,COUNT(CASE WHEN e.sal >= 1000 AND e.sal < 2000 THEN 1 END)   as "1"
+      ,COUNT(CASE WHEN e.sal < 1000 THEN 1 END)   as "0"
+  FROM emp e
+;
+SELECT SUBSTR(e.sal,1,1)
+      ,COUNT(*)
+  FROM emp e
+ GROUP BY  SUBSTR(e.sal,1,1)
+;
+-- 6. 직무별 급여 합의 단위를 구하고, 급여 합의 단위가 큰 순으로 정렬
+SELECT *
+  FROM emp e
+ GROUP BY SUBSTR((SELECT SUM(e.sal)
+                    FROM emp e
+                   GROUP BY e.job),1,2)
+;
+
+-- 7. 직무별 급여 평균이 2000이하인 경우를 구하고 평균이 높은 순으로 정렬
+SELECT e.job "직무"
+      ,AVG(e.sal) "급여 평균"
+  FROM emp e
+ GROUP BY e.job
+ HAVING AVG(e.sal) <= 2000
+ ORDER BY "급여 평균" DESC
+;
+/*
+직무,      급여 평균
+SALESMAN	1400
+CLERK	    1016.666666666666666666666666666666666667
+*/
+
+-- 8. 년도별 입사 인원을 구하시오
+SELECT SUBSTR(e.hiredate,1,2) "입사년도"
+      ,COUNT (*) 인원
+  FROM emp e
+ GROUP BY SUBSTR(e.hiredate,1,2)
+;
+/*
+입사년도, 인원
+82	        1
+            2
+80      	1
+81	       10
+*/
